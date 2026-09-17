ecbb6cc91302b9c56c8ebd0d1c3f976e5968bca9
Venkatesh <venkateshpurni@gmail.com>
2026-09-17 22:23:04 +0530

    Added more validation on Name field

--- src/main/java/com/vk/entity/Employee.java
+++ src/main/java/com/vk/entity/Employee.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,8 +21,8 @@ public class Employee {
     private Integer id;
 
     @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters.")
     private String name;
-
     @NotNull
     @Positive(message = "salary can't be Negative")
     private Long salary;
