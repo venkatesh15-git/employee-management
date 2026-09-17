5bb85e546d6a10a702cae7372316bec7fa4b1521
Venkatesh <venkateshpurni@gmail.com>
2026-09-17 12:02:50 +0530

    Add employee persistence layer

---
+++ src/main/java/com/vk/entity/Employee.java
@@ -0,0 +1,22 @@
+package com.vk.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name= "EmployeeDetailes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @SequenceGenerator(name="gen1",sequenceName="emp_tb1",initialValue=100,allocationSize=1)
+    @GeneratedValue(generator="gen1" ,strategy= GenerationType.SEQUENCE)
+    private Integer id;
+
+    private String name;
+
+    private String department;
+}
